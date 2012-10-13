076c6f9a668673057d42f6f0630976911fcd29b0
Aki Karkkainen <etimiti@gmail.com>
2012-10-13 14:00:38 +0200

    Edited config.rb, added Sass::Script::Number.precision option for more precise calculations.

--- webroot/assets/config.rb
+++ webroot/assets/config.rb
@@ -1,13 +1,14 @@
 # Require any additional compass plugins here.
 
-# Set this to the root of your project when deployed:
+# Assets paths
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
-# You can select your preferred output style here (can be overridden via the command line):
+# Other settings
+# Output style (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
 
@@ -17,9 +18,11 @@ output_style = :expanded
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# See https://github.com/davist11/nested-responsive-grid/blob/master/config.rb
+Sass::Script::Number.precision = 15
